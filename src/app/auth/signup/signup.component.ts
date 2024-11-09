8e3ec478c9204f3e3586d9942d482a771bd32290
Mathias Keulen <mathias.keulen@external.engie.com>
2024-11-09 21:51:42 +0100

    feat(reactive-forms): add signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,74 @@
 import { Component } from '@angular/core';
-
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signup = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur',
+    }),
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+        updateOn: 'change',
+      }),
+      passwordConfirm: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          // equalTo(contro),
+        ],
+        updateOn: 'change',
+      }),
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('teacher'),
+    agree: new FormControl(false, {
+      validators: [Validators.requiredTrue],
+    }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
+  });
+
+  onSubmit() {
+    console.log(this.signup);
+  }
+
+  onReset() {
+    this.signup.reset();
+  }
+}
